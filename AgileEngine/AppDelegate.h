b71e6a33ce213f6edf61b63b1623f2b79dde1cf9
Tymur Tkachenko <timurtkachenko@nest1.dlink>
2016-10-12 13:53:23 +0300

    Initial Commit

---
+++ AgileEngine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AgileEngine
+//
+//  Created by Tymur Tkachenko on 10/12/16.
+//  Copyright © 2016 Tymur Tkachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
